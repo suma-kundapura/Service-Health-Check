905da2a686f8d676b5d600c1153c9d80963bf38c
suma-kundapura <sumakundapuraa@gmail.com>
2025-01-27 08:59:19 +0530

    Initialize Spring Boot service project with Maven
    Set up a new Spring Boot application with Maven wrapper and required configurations. Includes initial application properties, .gitignore, Maven dependencies, and a basic test class.

---
+++ src/main/java/org/sd/servicehealthcheck/ServiceHealthCheckApplication.java
@@ -0,0 +1,13 @@
+package org.sd.servicehealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceHealthCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHealthCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sd/servicehealthcheck/ServiceHealthCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sd.servicehealthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHealthCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
